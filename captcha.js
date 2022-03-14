1eee9986f75c4945ac9cc26880e34636d91bab18
UsingQ <73696744+UsingQ@users.noreply.github.com>
2022-03-14 08:09:31 +0800

    111test

---
+++ captcha.js
@@ -0,0 +1,11 @@
+function Check(){
+    var captcha = new Array();
+    for (var i = 0; i < 9; ++i){ 
+        captcha[i] = Math.round(Math.random());
+    }
+    for (var i = 0; i < 9; ++i){ 
+        if(captcha[i] == 0){
+            
+        }
+    }
+}
