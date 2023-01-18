bc1d7ac6b20ead32453620eef0af2ccead24d728
using21qpaper <1316820747@qq.com>
2023-01-18 21:50:42 +0800

    53644554

--- captcha.js
+++ captcha.js
@@ -4,6 +4,11 @@ var randomPic = new Array();
 var vis = new Array(9).fill(0);
 var grade = 0;
 var ansNum = 0;
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n);
+    });
+}
 function refreshCaptcha(){
     randomVis.fill(0);
     vis.fill(0);
@@ -30,16 +35,17 @@ function refreshCaptcha(){
     }
 }
 function checkAns(a){
-    if(document.getElementById("c" + (a + 1)).getAttribute("style") == 'filter: opacity(50%)'){
+    if(document.getElementById("c" + (a + 1)).getAttribute("isPushed") == 'Yes'){
         if(captchaAns[a] == 1 && vis[a] == 1){
-            grade = (grade == 0) ? 0 : grade - 1;
+            grade--;
             vis[a] = 0;
         }
         if(captchaAns[a] == 0 && vis[a] == 1){
             grade++;
             vis[a] = 0;
         }
-        document.getElementById("c" + (a + 1)).setAttribute('style', 'background: url(images/' + captchaAns[a] + randomPic[a] + '.jpg); background-size:90px 90px;');
+        document.getElementById("c" + (a + 1)).setAttribute('style', 'background: url(images/' + captchaAns[a] + randomPic[a] + '.jpg); background-size:90px 90px;filter:brightness(100%); transform: scale(100%,100%);');
+        document.getElementById("c" + (a + 1)).setAttribute('isPushed', 'No');
     }
     else{
         if(captchaAns[a] == 1 && vis[a] == 0){
@@ -47,10 +53,11 @@ function checkAns(a){
             vis[a] = 1;
         }
         if(captchaAns[a] == 0 && vis[a] == 0){
-            grade = (grade == 0) ? 0 : grade - 1;
+            grade--;
             vis[a] = 1;
         }
-        document.getElementById("c" + (a + 1)).setAttribute('style', 'filter: opacity(50%)');
+        document.getElementById("c" + (a + 1)).setAttribute('style', 'background: url(images/' + captchaAns[a] + randomPic[a] + '.jpg); background-size:90px 90px;filter:brightness(50%); transform: scale(90%,90%);');
+        document.getElementById("c" + (a + 1)).setAttribute('isPushed', 'Yes');
     }
 }
 function showGrade(){
@@ -60,7 +67,8 @@ function showGrade(){
         document.getElementById("result").innerHTML = t;
     }
     else{
-        t = "你是不是主播，竟然没有答对！你的分数是：" + grade + "/" + ansNum;
+        t2 = (grade < 0) ? 0 : grade;
+        t = "你是不是主播，竟然没有答对！你的分数是：" + t2 + "/" + ansNum;
         document.getElementById("result").innerHTML = t;
     }
     refreshCaptcha();
